9a1c57a451b973b3f3f2036717dbf19854f9ca75
ErBot <botello@hawaii.edu>
2019-12-09 19:20:55 -1000

    Update Reviews.js

--- app/imports/api/reviews/Reviews.js
+++ app/imports/api/reviews/Reviews.js
@@ -2,8 +2,11 @@ import { Mongo } from 'meteor/mongo';
 import SimpleSchema from 'simpl-schema';
 import { Tracker } from 'meteor/tracker';
 
+/** The name of the collection and the global publication. */
+const reviewsName = 'Reviews';
+
 /** Define a Mongo collection to hold the data. */
-const Reviews = new Mongo.Collection('Reviews');
+const Reviews = new Mongo.Collection(reviewsName);
 
 /** Define a schema to specify the structure of each document in the collection. */
 const ReviewSchema = new SimpleSchema({
@@ -21,4 +24,4 @@ const ReviewSchema = new SimpleSchema({
 Reviews.attachSchema(ReviewSchema);
 
 /** Make the collection and schema available to other code. */
-export { Reviews, ReviewSchema };
+export { Reviews, ReviewSchema, reviewsName };
