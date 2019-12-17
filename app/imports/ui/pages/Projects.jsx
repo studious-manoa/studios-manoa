dead7fd5dadd09e99a829da0f71d5120eddf1b23
ErBot <botello@hawaii.edu>
2019-12-17 12:31:26 -1000

    Update Projects.jsx

--- app/imports/ui/pages/Projects.jsx
+++ app/imports/ui/pages/Projects.jsx
@@ -5,22 +5,20 @@ import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import { _ } from 'meteor/underscore';
 import { Link } from 'react-router-dom';
-import { Profiles, profilesName } from '../../api/profiles/Profiles';
-import { ProfilesProjects, profilesProjectsName } from '../../api/profiles/ProfilesProjects';
+import { profilesName } from '../../api/profiles/Profiles';
+import { profilesProjectsName } from '../../api/profiles/ProfilesProjects';
 import { Projects, projectsName } from '../../api/projects/Projects';
 import { ProjectsTags, projectsTagsName } from '../../api/projects/ProjectsTags';
 import MapLeaflet from '../components/MapLeaflet';
-import { Reviews, reviewsName } from '../../api/reviews/Reviews';
+import { ProjectsRatings, projectsRatingsValue } from '../../api/projects/ProjectsRatings';
 
 /** Gets the Project data as well as Profiles and Tags associated with the passed Project name. */
 function getProjectData(name) {
   const data = Projects.findOne({ name });
   const tags = _.pluck(ProjectsTags.find({ project: name }).fetch(), 'tag');
-  const ratings = _.pluck(Reviews.find({ project: name }).fetch(), 'rating');
+  const ratings = _.pluck(ProjectsRatings.find({ project: name }).fetch(), 'rating');
   const avgRating = ratings.reduce((a, b) => a + b, 0) / ratings.length;
-  const profiles = _.pluck(ProfilesProjects.find({ project: name }).fetch(), 'profile');
-  const profilePictures = profiles.map(profile => Profiles.findOne({ email: profile }).picture);
-  return _.extend({}, data, { tags, avgRating, participants: profilePictures });
+  return _.extend({}, data, { tags, avgRating });
 }
 
 /** Component for layout out a Project Card. */
@@ -105,7 +103,8 @@ export default withTracker(() => {
   const sub2 = Meteor.subscribe(projectsName);
   const sub3 = Meteor.subscribe(projectsTagsName);
   const sub4 = Meteor.subscribe(profilesName);
-  const sub5 = Meteor.subscribe(reviewsName);
+  const sub5 = Meteor.subscribe(projectsRatingsValue);
+
   return {
     ready: sub1.ready() && sub2.ready() && sub3.ready() && sub4.ready() && sub5.ready(),
   };
